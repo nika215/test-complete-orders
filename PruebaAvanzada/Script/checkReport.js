24d948846ca653e0064a1c5cf257636dc4083275
Monica Antona <monica.antona-crespo@capgemini.com>
2024-11-02 12:17:20 +0100

    Add check report

---
+++ PruebaAvanzada/Script/checkReport.js
@@ -0,0 +1,40 @@
+﻿
+var filePath= Project.Path+"customerList.txt";
+
+function checkReport(){
+
+  // Remove previus file if exists
+  if(aqFileSystem.Exists(filePath)){
+      aqFileSystem.DeleteFile(filePath);
+  }
+  
+
+  //open orders app
+  TestedApps.Orders.Run();
+  //open the file from the report will be generated
+  openFile();
+  //create the report file
+  generateReport();
+  
+  // check if the generated report is equals to the expected file
+  Files.CustomerList_txt.Check(filePath);
+  
+  TestedApps.Orders.Terminate();
+}
+
+function openFile(){
+  //Moves the mouse cursor to the menu item specified and then simulates a single click.
+  Aliases.Orders.MainForm.MainMenu.Click("File|Open...");
+  //Clicks the '|Desktop|Desktop' item of the 'tvNamespaceTreeControl' tree.
+  Aliases.Orders.dlgOpen.OpenFile(Project.Path+"Stores\\Files\\multipleClients.tbl", "Table (*.tbl)");
+}
+
+function generateReport(){
+Aliases.explorer.wndShell_TrayWnd.DesktopWindowXamlSource.Click(963, 28);
+  let orders = Aliases.Orders;
+  orders.MainForm.MainMenu.Click("Report|Generate customer list...");
+  let dlgSaveAs = orders.dlgSaveAs;
+  dlgSaveAs.DUIViewWndClassName.Explorer_Pane.FloatNotifySink.ComboBox.SetText(filePath);
+  dlgSaveAs.btnSave.ClickButton();
+}
+
--- PruebaAvanzada/Script/doNotSaveEmpty.js
+++ PruebaAvanzada/Script/doNotSaveEmpty.js
@@ -2,10 +2,19 @@
 {
   //Runs the "Orders" tested application.
   TestedApps.Orders.Run();
+  //Clicks the 1 item of the 'ToolBar' toolbar.
+  Aliases.Orders.MainForm.ToolBar.ClickItem(1, false);
+  //Opens the 'C:\Users\moantona\OneDrive - Capgemini\Documents\TestComplete 15 Projects\PruebaAvanzada\PruebaAvanzada\Stores\Files\PepeOrder.tbl' file via the 'dlgOpen' dialog.
+  Aliases.Orders.dlgOpen.OpenFile(Project.path+"Stores\\Files\\PepeOrder.tbl", "Table (*.tbl)");
   //Moves the mouse cursor to the menu item specified and then simulates a single click.
   Aliases.Orders.MainForm.MainMenu.Click("Orders|New order...");
   //Clicks the 'ButtonOK' button.
   Aliases.Orders.OrderForm.ButtonOK.ClickButton();
   //Clicks the 'MainForm' object.
   Aliases.Orders.MainForm.Click(66, 45);
+    //Compares the OrdersView Stores item with the contents of the control.
+  
+    equal(Tables.OrdersView.RowCount,1);
+  
+  TestedApps.Orders.Terminate();
 }
